87139306e3c165cea9fcc6b6821003756609c782
Skippy <philip@mcgaw.eu>
2026-02-16 22:33:01 +0000

    .

--- BackButtons/Arduino/Arduino.ino
+++ BackButtons/Arduino/Arduino.ino
@@ -1,23 +1,46 @@
 #include "Charlieplex.h"
 
-byte pins[] = {1, 2, 3, 4};
-Charlieplex charlie(pins, sizeof(pins));
 
 
+#define LED_A1 0
+#define LED_A2 1
+#define LED_A3 2
+#define LED_A4 3
+
+#define LED_B1 4
+#define LED_B2 5
+#define LED_B3 6
+#define LED_B4 7
+
+#define LED_C1 8
+#define LED_C2 9
+#define LED_C3 10
+#define LED_C4 11
+
+bool LED_A1_Status, LED_A2_Status, LED_A3_Status, LED_A4_Status  = false;
+bool LED_B1_Status, LED_B2_Status, LED_B3_Status, LED_B4_Status  = false;
+bool LED_C1_Status, LED_C2_Status, LED_C3_Status, LED_C4_Status  = false;
+
+bool Switch_A1_Status, Switch_A2_Status, Switch_A3_Status, Switch_A4_Status  = false;
+bool Switch_B1_Status, Switch_B2_Status, Switch_B3_Status, Switch_B4_Status  = false;
+bool Switch_C1_Status, Switch_C2_Status, Switch_C3_Status, Switch_C4_Status  = false;
+
+byte LED_pins[] = {1, 2, 3, 4};
+Charlieplex charlie(LED_pins, sizeof(LED_pins));
 
 void setup() {
-    charlie.setLed(0, false);    // Turn off LED0
-    charlie.setLed(1, false);    // Turn off LED1
-    charlie.setLed(2, false);    // Turn off LED2
-    charlie.setLed(3, false);    // Turn off LED3
-    charlie.setLed(4, false);    // Turn off LED4
-    charlie.setLed(5, false);    // Turn off LED5
-    charlie.setLed(6, false);    // Turn off LED6
-    charlie.setLed(7, false);    // Turn off LED7
-    charlie.setLed(8, false);    // Turn off LED8
-    charlie.setLed(9, false);    // Turn off LED9
-    charlie.setLed(10, false);    // Turn off LED10
-    charlie.setLed(11, false);    // Turn off LED11
+    charlie.setLed(LED_A1, LED_A1_Status);    // Turn off LED_A1
+    charlie.setLed(LED_A2, LED_A2_Status);    // Turn off LED_A2
+    charlie.setLed(LED_A3, LED_A3_Status);    // Turn off LED_A3
+    charlie.setLed(LED_A4, LED_A4_Status);    // Turn off LED_A4
+    charlie.setLed(LED_B1, LED_B1_Status);    // Turn off LED_B1
+    charlie.setLed(LED_B2, LED_B2_Status);    // Turn off LED_B2
+    charlie.setLed(LED_B3, LED_B3_Status);    // Turn off LED_B3
+    charlie.setLed(LED_B4, LED_B4_Status);    // Turn off LED_B4
+    charlie.setLed(LED_C1, LED_C1_Status);    // Turn off LED_C1
+    charlie.setLed(LED_C2, LED_C2_Status);    // Turn off LED_C2
+    charlie.setLed(LED_C3, LED_C3_Status);    // Turn off LED_C3
+    charlie.setLed(LED_C4, LED_C4_Status);    // Turn off LED_C4
 
 }
 void loop() {
--- BackButtons/Arduino/Charlieplex.h
+++ BackButtons/Arduino/Charlieplex.h
@@ -20,112 +20,40 @@
   * DEALINGS IN THE SOFTWARE.
 */
 
-#include "Charlieplex.h"
-#if defined(ARDUINO) && ARDUINO >= 100
-#include <Arduino.h>
-#else
-#include <WProgram.h>
+#ifndef Charlieplex_h
+#define Charlieplex_h
+    
+#include <inttypes.h>
+
+class Charlieplex
+{
+  public:
+    Charlieplex(const uint8_t *pins, uint8_t numPins);
+    ~Charlieplex();
+    
+    int count() const { return _count; }
+    
+    bool led(int index) const { return _values[index] != 0; }
+    void setLed(int index, bool value) { _values[index] = (value ? 255 : 0); }
+
+    uint8_t pwmLed(int index) const { return _values[index]; }
+    void setPwmLed(int index, uint8_t value) { _values[index] = value; }
+    
+    unsigned long holdTime() const { return _holdTime; }
+    void setHoldTime(unsigned long us) { _holdTime = us; }
+    
+    void loop();
+    void refresh();
+    
+  private:
+    int _count;
+    uint8_t *_pins1;
+    uint8_t *_pins2;
+    uint8_t *_values;
+    unsigned long _holdTime;
+    unsigned long _lastTime;
+    int _currentIndex;
+    uint8_t _pwmPhase;
+
+};
 #endif
-#include <stdlib.h>
-#include <string.h>
-
-Charlieplex::Charlieplex(const uint8_t *pins, uint8_t numPins)
-  : _count(((int)numPins) * (numPins - 1))
-  , _lastTime(micros())
-  , _currentIndex(-1)
-  , _pwmPhase(0xC0)
-  {
-    // Determine the best hold time for 50 Hz refresh when all LED's are lit.  Divide it again by 4 (to get 200 Hz) to manage the simulated PWM in refresh().
-    _holdTime = 20000 / _count / 4;
-    
-    // Allocate the pin arrays and populate them.  Doing this now makes refresh() more efficient later, at the expense of some memory.
-    _pins1 = (uint8_t *)malloc(_count);
-    _pins2 = (uint8_t *)malloc(_count);
-    int n = 0;
-    for (uint8_t pass = 1; pass < numPins; ++pass) {
-      for (uint8_t pin = 0; pin < (numPins - pass); ++pin) {
-        _pins1[n] = _pins2[n + 1] = pins[pin];
-        _pins2[n] = _pins1[n + 1] = pins[pin + pass];
-        n += 2;
-      }
-    }
-
-    // Allocate space for the LED value array and zero it.
-    _values = (uint8_t *)malloc(_count);
-    memset(_values, 0, _count);
-    
-    // Start with all pins configured as floating inputs (all LED's off).
-    for (uint8_t pin = 0; pin < numPins; ++pin) {
-      digitalWrite(pins[pin], LOW);
-      pinMode(pins[pin], INPUT);
-    }
-  }
-  
-  Charlieplex::~Charlieplex()
-  {
-    free(_pins1);
-    free(_pins2);
-    free(_values);
-  }
-  
-  void Charlieplex::loop()
-  {
-    unsigned long us = micros();
-    if ((us - _lastTime) >= _holdTime) {
-      _lastTime = us;
-      refresh();
-    }
-  }
-  
-  void Charlieplex::refresh()
-  {
-    // Find the next LED to be lit.
-    int prevIndex = _currentIndex;
-    int limit = _count;
-    while (limit >= 0) {
-      _currentIndex = (_currentIndex + 1) % _count;
-      if (_values[_currentIndex] != 0)
-      break;
-      --limit;
-    }
-    if (limit < 0) {
-      // No LED's are lit.  Turn off the previous LED and exit.
-      if (prevIndex != -1) {
-        digitalWrite(_pins1[prevIndex], LOW);
-        digitalWrite(_pins2[prevIndex], LOW);
-        pinMode(_pins1[prevIndex], INPUT);
-        pinMode(_pins2[prevIndex], INPUT);
-      }
-      _currentIndex = -1;
-      return;
-    }
-    
-    // Light the current LED.
-    uint8_t value = _values[_currentIndex];
-    uint8_t pin1 = _pins1[_currentIndex];
-    uint8_t pin2 = _pins2[_currentIndex];
-    _pwmPhase += 0x40;
-    if (prevIndex != _currentIndex) {
-      // Turn off the previous LED.
-      if (prevIndex != -1) {
-        digitalWrite(_pins1[prevIndex], LOW);
-        digitalWrite(_pins2[prevIndex], LOW);
-        pinMode(_pins1[prevIndex], INPUT);
-        pinMode(_pins2[prevIndex], INPUT);
-      }
-      
-      // We simulate PWM using a phase counter because analogWrite() combined with holdTime() causes too much flickering if more than one LED is lit.  This reduces the PWM resolution to 1 in 4.
-      pinMode(pin1, OUTPUT);
-      pinMode(pin2, OUTPUT);
-      if (value > _pwmPhase)
-        digitalWrite(pin1, HIGH);
-      else
-      digitalWrite(pin1, LOW);
-    } else {
-      // Same LED as previous.  Since there is only a single LED that is lit, we can use analogWrite() to set the PWM state.
-      if (value == 255)
-        digitalWrite(pin1, HIGH);
-      else
-        analogWrite(pin1, value);
-    }
-  }
